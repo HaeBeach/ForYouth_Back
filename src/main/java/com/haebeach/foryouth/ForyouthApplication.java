020d31505819b51456262493ece16d647063142b
djeon <daewoong.jeon@kt.com>
2023-01-24 13:37:53 +0900

    first commit

---
+++ src/main/java/com/haebeach/foryouth/ForyouthApplication.java
@@ -0,0 +1,13 @@
+package com.haebeach.foryouth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForyouthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForyouthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haebeach/foryouth/ForyouthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haebeach.foryouth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForyouthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
